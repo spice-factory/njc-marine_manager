3ebe14347363e12d20459476ace521a54d99b397
akinobu_abe <akinobu_abe@spice-factory.co.jp>
2024-07-25 03:37:53 +0900

    ヘッダーのテキストを変更

--- header.php
+++ header.php
@@ -75,7 +75,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             <a class="globalNaviMain__item-link" href="<?php echo site_url(); ?>/about/">私たちの想い</a>
           </li>
           <li class="globalNaviMain__item <?php echo ($page_slug == 'solution') ? 'is-current' : ''; ?>">
-            <a class="globalNaviMain__item-link" href="<?php echo site_url(); ?>/solution/">+reC.にできること</a>
+            <a class="globalNaviMain__item-link" href="<?php echo site_url(); ?>/solution/">プラスレックにできること</a>
           </li>
           <li class="globalNaviMain__item <?php echo ($post_archive_slug == 'magazine') ? 'is-current' : ''; ?>">
             <a class="globalNaviMain__item-link" href="<?php echo site_url(); ?>/magazine/">マガジン</a>
